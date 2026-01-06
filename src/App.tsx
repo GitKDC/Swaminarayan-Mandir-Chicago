f080512318387e0dc1fedffd12edccc48fc53d98
kartikchaudhari <kartikchaudhari246@gmail.com>
2026-01-06 14:33:35 +0530

    Removed errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HeroSection from '../src/components/HeroSection'
 import './App.css'
 import AboutPage from './pages/AboutPage';
